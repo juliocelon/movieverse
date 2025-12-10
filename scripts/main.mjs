5b77a66dc29bea8655fbf761eb35263656352f95
Julio Cesar Celon Altamirano <juliocelon@ibm.com>
2025-12-10 13:00:23 -0600

    get api key

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -21,3 +21,19 @@ document.addEventListener('DOMContentLoaded', () => {
         actionLink.classList.add('active');
     }
 });
+
+async function obtenerDatosSeguros() {
+    // Llama al endpoint generado por Netlify para tu función
+    console.log('EMPEZANDO');
+  const response = await fetch('./get-api-key.js');
+  console.log('AQUI');
+  if (!response.ok) {
+    throw new Error('Error al llamar a la función segura');
+  }
+console.log('VOY');
+    const data = await response.json();
+    console.log('YA FUE');
+  console.log('Datos obtenidos de forma segura:', data);
+}
+
+obtenerDatosSeguros();
