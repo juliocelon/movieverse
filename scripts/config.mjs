d569ec66c8f10cffc493a6134cf15fa2a0ea7af8
Julio Cesar Celon Altamirano <juliocelon@ibm.com>
2025-12-10 14:15:41 -0600

    Fixing issues

--- scripts/config.mjs
+++ scripts/config.mjs
@@ -5,9 +5,6 @@ export const DEFAULT_MOVIE_COUNT = 7;
 export const FAVORITES_KEY = 'favoriteMovieIds';
 export const LANGUAGE = "en-US"; 
 
-export const YOUTUBE_API_KEY = "AIzaSyDNF0G3pkLFMdxDhPl81ZY9D1gLEbgYpk0"; 
-export const YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3/search";
+export const YOUTUBE_API_KEY = "AIzaSyD_eW6kc3duCdXSZe8l-fXBNUUaNcfkxS4"; 
 
-//const key = import.meta.env.VITE_YOUTUBE_API_KEY;
-//export const YT_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
-//console.log('Mi Key:', YT_KEY);
+export const YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3/search";
